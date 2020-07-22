be709a546eb18d1a86bf3b637b370db02fb664f4
John Behan <behanj@gmail.com>
2020-07-22 23:15:56 +0100

    update map and add constituency data

---
+++ data/constituencies.js
@@ -0,0 +1,158 @@
+export default {
+  "carlow-kilkenny": {
+    name: "Carlow-Kilkenny",
+    seats: 5,
+  },
+  "cavan-monaghan": {
+    name: "Cavan-Monaghan",
+    seats: 5,
+  },
+  clare: {
+    name: "Clare",
+    seats: 4,
+  },
+  "cork-east": {
+    name: "Cork East",
+    seats: 4,
+  },
+  "cork-north-central": {
+    name: "Cork North-Central",
+    seats: 4,
+  },
+  "cork-north-west": {
+    name: "Cork North-West",
+    seats: 3,
+  },
+  "cork-south-central": {
+    name: "Cork South-Central",
+    seats: 4,
+  },
+  "cork-south-west": {
+    name: "Cork South-West",
+    seats: 3,
+  },
+  donegal: {
+    name: "Donegal",
+    seats: 5,
+  },
+  "dublin-bay-north": {
+    name: "Dublin Bay North",
+    seats: 5,
+  },
+  "dublin-bay-south": {
+    name: "Dublin Bay South",
+    seats: 4,
+  },
+  "dublin-central": {
+    name: "Dublin Central",
+    seats: 4,
+  },
+  "dublin-fingal": {
+    name: "Dublin Fingal",
+    seats: 5,
+  },
+  "dublin-mid-west": {
+    name: "Dublin Mid-West",
+    seats: 4,
+  },
+  "dublin-north-west": {
+    name: "Dublin North-West",
+    seats: 3,
+  },
+  "dublin-rathdown": {
+    name: "Dublin Rathdown",
+    seats: 3,
+  },
+  "dublin-south-central": {
+    name: "Dublin South-Central",
+    seats: 4,
+  },
+  "dublin-south-west": {
+    name: "Dublin South-West",
+    seats: 5,
+  },
+  "dublin-west": {
+    name: "Dublin West",
+    seats: 4,
+  },
+  "dun-laoghaire": {
+    name: "Dún Laoghaire",
+    seats: 4,
+  },
+  "galway-east": {
+    name: "Galway East",
+    seats: 3,
+  },
+  "galway-west": {
+    name: "Galway West",
+    seats: 5,
+  },
+  kerry: {
+    name: "Kerry",
+    seats: 5,
+  },
+  "kildare-north": {
+    name: "Kildare North",
+    seats: 4,
+  },
+  "kildare-south": {
+    name: "Kildare South",
+    seats: 4,
+  },
+  "laois-offaly": {
+    name: "Laois–Offaly",
+    seats: 5,
+  },
+  "limerick-city": {
+    name: "Limerick City",
+    seats: 4,
+  },
+  "limerick-county": {
+    name: "Limerick County",
+    seats: 3,
+  },
+  "longford-westmeath": {
+    name: "Longford–Westmeath",
+    seats: 4,
+  },
+  louth: {
+    name: "Louth",
+    seats: 5,
+  },
+  mayo: {
+    name: "Mayo",
+    seats: 4,
+  },
+  "meath-east": {
+    name: "Meath East",
+    seats: 3,
+  },
+  "meath-west": {
+    name: "Meath West",
+    seats: 3,
+  },
+  "roscommon-galway": {
+    name: "Roscommon–Galway",
+    seats: 3,
+  },
+  "sligo-leitrim": {
+    name: "Sligo–Leitrim",
+    seats: 4,
+  },
+  tipperary: {
+    name: "Tipperary",
+    seats: 5,
+  },
+  waterford: {
+    name: "Waterford",
+    seats: 4,
+  },
+  wexford: {
+    name: "Wexford",
+    seats: 5,
+  },
+  wicklow: {
+    name: "Wicklow",
+    setas: 5,
+  },
+};
