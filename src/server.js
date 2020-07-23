a5fbcbfabdc9913542f714c860ec4b4ac7fe56e0
John Behan <behanj@gmail.com>
2020-07-23 20:36:56 +0100

    update export server path

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const dev = NODE_ENV === "development";
 
 polka() // You can also use Express
   .use(
-    "How-They-Voted-Ireland/client-app",
+    "client-app",
     compression({ threshold: 0 }),
     sirv("static", { dev }),
     sapper.middleware()
