a13d40d5e9a8739e1dc90a9701a76c0639271a1b
John Behan <behanj@gmail.com>
2020-07-25 22:09:32 +0100

    add basic meta info

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,3 +1,8 @@
+<svelte:head>
+  <title>How They Voted Ireland</title>
+  <meta name="description" content="Under development - An app to allow Irish people to easily see how their TDs have voted on bills in the Dáil">
+</svelte:head>
+
 <main>
 	<slot></slot>
 </main>
